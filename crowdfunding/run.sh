a41b2b847c2e5e3ff0eb2fd51d037b495961b3ed
Ktech <ktechgau@gmail.com>
2024-01-21 20:15:40 +0800

    ammended run.sh to try and fix an error

--- crowdfunding/run.sh
+++ crowdfunding/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 python3 manage.py migrate
 python3 manage.py createsuperuser --no-input
-gunicorn --bind :8000 - -workers 1 crowdfunding.wsgi
+gunicorn --bind :8000 --workers 1 crowdfunding.wsgi
