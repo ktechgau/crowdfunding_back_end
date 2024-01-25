4415c1f79c23cebe74caf081b1b3f55d429721a3
Ktech <ktechgau@gmail.com>
2024-01-25 13:08:41 +0800

    Added update function for users

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -10,3 +10,10 @@ class CustomUserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         return CustomUser.objects.create_user(**validated_data)
 
+class CustomUserDetailSerializer(CustomUserSerializer):
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.email = validated_data.get('email', instance.email)
+        return instance
--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -1,11 +1,14 @@
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 from .models import CustomUser
-from .serializers import CustomUserSerializer
+from .serializers import CustomUserSerializer, CustomUserDetailSerializer
+from.permissions import IsOwnerOrReadOnlyUser
 
 class CustomUserList(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnlyUser]
+    
     def get(self, request):
         users = CustomUser.objects.all()
         serializer = CustomUserSerializer(users, many=True)
@@ -19,6 +22,7 @@ class CustomUserList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class CustomUserDetail(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnlyUser]
     def get_object(self, pk):
         try:
             return CustomUser.objects.get(pk=pk)
@@ -29,3 +33,19 @@ class CustomUserDetail(APIView):
         user = self.get_object(pk)
         serializer = CustomUserSerializer(user)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        user = self.get_object(pk)
+        serializer = CustomUserDetailSerializer(
+            instance=user,
+            data=request.data,
+            partial=True
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
