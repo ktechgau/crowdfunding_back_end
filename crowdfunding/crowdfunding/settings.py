4ae8c8d3737a53dea9f0bf41ffb2a8f1df077b06
Ktech <ktechgau@gmail.com>
2024-01-26 21:45:20 +0800

    deleted comments from settings

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'crowdfunding.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
+       
         'NAME': os.environ.get('DATABASE_DIR', BASE_DIR / 'db.sqlite3'),
     }
 }
