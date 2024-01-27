ce31b881c9147f82610c6837d496d6f54d75480a
Ktech <ktechgau@gmail.com>
2024-01-27 12:02:06 +0800

    removed categories

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -6,39 +6,39 @@ from django.db.models import Sum
 
 class Project(models.Model):
 
-   CATEGORY_CHOICES=[
-      ("AGRI", 'Agriculture'),
-      ("ARCHITECTURE", 'Architecture, Construction & Planning'),
-      ("BUSINESS", 'Business Management & Entrepreneurship'),
-      ("CREATIVE", 'Creative arts & Design'),
-      ("EDUCATION", 'Teaching, Education & Training'),
-      ("ENGINEERING", 'Engineering, Automation & Technology'),
-      ("ENVIRONMENTAL", 'Environmental Science & Sustainability'),
-      ("HEALTH", 'Health, Medicine, Psychology'),
-      ("IT", 'IT and Computing'),
-      ("LAW", 'Law, Paralegal'),
-      ("MEDIA", 'Media & Communications'),
-      ("PERSONAL", 'Personal Care, Beauty & Hair, Fitness'),
-      ("SCIENCES", 'Sciences and Mathematics'),
-      ("SOCIETY", 'Society, Culture & Humanities'),
-      ("TRAVEL", 'Travel, Tourism & Hospitality'),
-      ("VET", 'Veterinary Medicine'),
-      ("OTHER", 'Other'),
-   ]
+#    CATEGORY_CHOICES=[
+#       ("AGRI", 'Agriculture'),
+#       ("ARCHITECTURE", 'Architecture, Construction & Planning'),
+#       ("BUSINESS", 'Business Management & Entrepreneurship'),
+#       ("CREATIVE", 'Creative arts & Design'),
+#       ("EDUCATION", 'Teaching, Education & Training'),
+#       ("ENGINEERING", 'Engineering, Automation & Technology'),
+#       ("ENVIRONMENTAL", 'Environmental Science & Sustainability'),
+#       ("HEALTH", 'Health, Medicine, Psychology'),
+#       ("IT", 'IT and Computing'),
+#       ("LAW", 'Law, Paralegal'),
+#       ("MEDIA", 'Media & Communications'),
+#       ("PERSONAL", 'Personal Care, Beauty & Hair, Fitness'),
+#       ("SCIENCES", 'Sciences and Mathematics'),
+#       ("SOCIETY", 'Society, Culture & Humanities'),
+#       ("TRAVEL", 'Travel, Tourism & Hospitality'),
+#       ("VET", 'Veterinary Medicine'),
+#       ("OTHER", 'Other'),
+#    ]
 
-   title = models.CharField(max_length=200)
-   description = models.TextField()
-   goal = models.IntegerField()
-   image = models.URLField()
-   is_open = models.BooleanField()
-   date_created = models.DateTimeField()
-   category = models.CharField(max_length=200, choices=CATEGORY_CHOICES, default="AGRI")
-   owner = models.ForeignKey(
-    get_user_model(),
-    on_delete=models.CASCADE,
-    related_name='owned_projects'
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    goal = models.IntegerField()
+    image = models.URLField()
+    is_open = models.BooleanField()
+    date_created = models.DateTimeField()
+#    category = models.CharField(max_length=200, choices=CATEGORY_CHOICES, default="AGRI")
+    owner = models.ForeignKey(
+     get_user_model(),
+     on_delete=models.CASCADE,
+     related_name='owned_projects'
    
- )
+  )
    
 
 class Pledge(models.Model):
